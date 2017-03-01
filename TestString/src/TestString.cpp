e912ea6685a8bb9a42a258d3a6f6f5d274fae3b7
jbotero <jonathanboterog@gmail.com>
2017-03-01 18:39:09 -0500

    version libreria v01.03

--- TestString/src/TestString.cpp
+++ TestString/src/TestString.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : TestString.cpp
 // Author      : jbotero
-// Version     : 01.02
+// Version     : 01.03
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
